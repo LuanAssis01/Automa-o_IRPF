43b61c2678c6ec9f7219642b610146062f08285e
LuanAssis01 <assisluan863@gmail.com>
2025-05-08 08:10:49 -0300

    fix: ajusted for windows

--- exc.py
+++ exc.py
@@ -8,19 +8,19 @@ pyautogui.press("win")
 pyautogui.write("IRPF 2025")
 pyautogui.press("enter")
 
-time.sleep(2)
+time.sleep(5)
 
 # Clica no botão (Declaração)
-pyautogui.moveTo(x=22, y=77, duration=1)
-pyautogui.click(x=22, y=77, button="left", clicks=1)
+pyautogui.moveTo(x=48, y=34, duration=1)
+pyautogui.click(x=48, y=34, button="left", clicks=1)
 
 # Clica no botão (Novo)
-pyautogui.moveTo(x=44, y=102, duration=1)
-pyautogui.click(x=44, y=102, button="left", clicks=1)
+pyautogui.moveTo(x=37, y=61, duration=1)
+pyautogui.click(x=37, y=61, button="left", clicks=1)
 
 # Clica no botão (Iniciar declaração em branco)
-pyautogui.moveTo(x=1261, y=678, duration=1)
-pyautogui.click(x=1261, y=678, button="left", clicks=1)
+pyautogui.moveTo(x=1244, y=635, duration=1)
+pyautogui.click(x=1244, y=635, button="left", clicks=1)
 
 # Troca para a aba do navegador
 # pyautogui.hotkey("alt", "tab") 
@@ -28,18 +28,16 @@ pyautogui.press("win")
 pyautogui.write("firefox")
 pyautogui.press("enter")
 
-# Aperta na aba do site gerador de CPF
-pyautogui.moveTo(x=150, y=57, duration=1)
-pyautogui.click(x=150, y=57, button="left", clicks=1)
-
+pyautogui.write("https://www.4devs.com.br/gerador_de_cpf")
+pyautogui.press("enter")
 
 # Aperta em gerar CPF
-pyautogui.moveTo(x=1100, y=496, duration=1)
-pyautogui.click(x=1100, y=496, button="left", clicks=1)
+pyautogui.moveTo(x=1094, y=419, duration=1)
+pyautogui.click(x=1094, y=419, button="left", clicks=1)
 
 # Copia o CPF gerado
-pyautogui.moveTo(x=1062, y=579, duration=1)
-pyautogui.click(x=1062, y=579, button="left", clicks=1)
+pyautogui.moveTo(x=1061, y=512, duration=1)
+pyautogui.click(x=1061, y=512, button="left", clicks=1)
 
 time.sleep(2)
 
@@ -49,8 +47,8 @@ pyautogui.hotkey("alt", "tab")
 time.sleep(2)
 
 # Colo o CPF gerado
-pyautogui.moveTo(x=794, y=786, duration=1)
-pyautogui.click(x=794, y=786, button="left", clicks=1)
+pyautogui.moveTo(x=800, y=741, duration=1)
+pyautogui.click(x=800, y=741, button="left", clicks=1)
 pyautogui.hotkey("ctrl", "v") 
 
 time.sleep(2)
@@ -65,35 +63,40 @@ pyautogui.hotkey("enter")
 time.sleep(2)
 pyautogui.hotkey("enter")
 
+time.sleep(2)
+
 # Troca para a aba do navegador
 pyautogui.hotkey("alt", "tab")
 
-# Aperta na aba do CNPJ
-pyautogui.moveTo(x=262, y=56, duration=1)
-pyautogui.click(x=262, y=56, button="left", clicks=1)
+time.sleep(2)
+
+# Abro a aba do gerador de CNPJ
+pyautogui.hotkey("ctrl", "t")
+pyautogui.write("https://www.4devs.com.br/gerador_de_cnpj")
+pyautogui.hotkey("enter")
 
 # Aperta em gerar CNPJ
-pyautogui.moveTo(x=1102, y=460, duration=1)
-pyautogui.click(x=1102, y=460, button="left", clicks=1)
+pyautogui.moveTo(x=1083, y=390, duration=1)
+pyautogui.click(x=1083, y=390, button="left", clicks=1)
 
 # Copia o CNPJ gerado
-pyautogui.moveTo(x=1057, y=555, duration=1)
-pyautogui.click(x=1057, y=555, button="left", clicks=1)
+pyautogui.moveTo(x=1064, y=483, duration=1)
+pyautogui.click(x=1064, y=483, button="left", clicks=1)
 
 # Volta para o programa
 pyautogui.hotkey("alt", "tab") 
 
 # Aperto em Rendimentos Tributáveis REcebidos PJ
-pyautogui.moveTo(x=824, y=252, duration=1)
-pyautogui.click(x=824, y=252, button="left", clicks=1)
+pyautogui.moveTo(x=836, y=2722, duration=1)
+pyautogui.click(x=836, y=272, button="left", clicks=1)
 
 # Aperto em Novo
-pyautogui.moveTo(x=1645, y=999, duration=1)
-pyautogui.click(x=1645, y=999, button="left", clicks=1)
+pyautogui.moveTo(x=1649, y=953, duration=1)
+pyautogui.click(x=1649, y=953, button="left", clicks=1)
 
 # Colo o CNPJ gerado
-pyautogui.moveTo(x=510, y=224, duration=1)
-pyautogui.click(x=510, y=224, button="left", clicks=1)
+pyautogui.moveTo(x=448, y=279, duration=1)
+pyautogui.click(x=448, y=279, button="left", clicks=1)
 pyautogui.hotkey("ctrl", "v") 
 
 # Insiro o nome da fonte pagadora
@@ -101,30 +104,32 @@ pyautogui.hotkey("tab")
 pyautogui.write("CEIA")
 
 # Aperto em OK
-pyautogui.moveTo(x=1652, y=1059, duration=1)
-pyautogui.click(x=1652, y=1059, button="left", clicks=1)
+pyautogui.moveTo(x=1650, y=1007, duration=1)
+pyautogui.click(x=1650, y=1007, button="left", clicks=1)
 
-# Aperto em Importar arquivo
-pyautogui.moveTo(x=1656, y=1058, duration=1)
-pyautogui.click(x=1656, y=1058, button="left", clicks=1)
+# Volto em Inicio
+pyautogui.moveTo(x=414, y=98, duration=1)
+pyautogui.click(x=414, y=98, button="left", clicks=1)
+
+# Aperto em Imprimir declaração
+pyautogui.moveTo(x=1224, y=296, duration=1)
+pyautogui.click(x=1224, y=296, button="left", clicks=1)
 
 # Aperto em Ok
-pyautogui.moveTo(x=1086, y=724, duration=1)
-pyautogui.click(x=1086, y=724, button="left", clicks=1)
+pyautogui.moveTo(x=1120, y=484, duration=1)
+pyautogui.click(x=1120, y=484, button="left", clicks=1)
 
+time.sleep(5)
 
+# Clico no icone de salvamento
+pyautogui.moveTo(x=478, y=219, duration=1)
+pyautogui.click(x=478, y=219, button="left", clicks=2)
 
+time.sleep(3)
 
-
-
-
-
-
-
-
-
-
-
+# Aperto em salvar
+pyautogui.moveTo(x=1093, y=699, duration=1)
+pyautogui.click(x=1093, y=699, button="left", clicks=1)
 
 
 
